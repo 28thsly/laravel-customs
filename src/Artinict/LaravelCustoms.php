bbe3509303e81b350b5b01d136af0e301e8c6de2
Sadiq Lukman <28thsly@gmail.com>
2019-03-08 18:44:49 +0100

    laravel-customs is up and running

---
+++ src/Artinict/LaravelCustoms.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Artinict;
+
+
+class LaravelCustoms
+{
+
+  public static function __callStatic($class, $args)
+  {
+
+    $currentDir = dirname(__FILE__);
+    $baseDir = dirname(dirname($currentDir));
+
+    //Load customs configuration file
+    $cusfigurationPath = $baseDir.'\\config\\cusfiguration.php';
+    if(! file_exists($cusfigurationPath) )
+      if(! @copy($currentDir.'\\cusfiguration.php', $cusfigurationPath))
+          throw new \Exception("Cusfiguration.php NOT FOUND. Kindly reinstall the Laravel customs package.");
+
+    $cusfiguration = require $cusfigurationPath;
+
+    $APP_NAME = $cusfiguration['LC_APP_NAMESPACE'];
+    $appDir = $baseDir.'\\'.$APP_NAME;
+
+    //If class doesn't exist in the global array,
+    if( ! @array_key_exists($class, $cusfiguration)){
+
+      //Check for prefix and break down it down
+      $pou = strrpos($class, '_');
+      $prefix = substr($class, 0, $pou);
+      $alias = substr($class, $pou + 1);
+
+      //If prefix exists(Underscore)
+      if( $prefix !== false){
+
+        //if the Prefix exists in the global config
+        if( @array_key_exists($prefix, $cusfiguration)){
+
+          //if the Alias exists in the prefix config,
+          if(array_key_exists($alias, $cusfiguration[$prefix]))
+              $className = $cusfiguration[$prefix][$alias];
+          else
+            $className = static::autoResolveClassname($appDir, $appDir, $APP_NAME, $class);
+
+        }
+        else{
+          $className = static::autoResolveClassname($appDir, $appDir, $APP_NAME, $class);
+        }
+
+      }
+      else{
+        $className = static::autoResolveClassname($appDir, $appDir, $APP_NAME, $class);
+      }
+
+    }
+    else{
+      $className = $cusfiguration[$class];
+    }
+
+    if($className === null)
+      throw new \Exception("Class \"$class\" cannot be found. Add an alias: \"$class\" => Classname::class in config/cusfiguration.php");
+
+    //If argument is NULL, it means the user just wants the class instance
+    if($args == null)
+      return $className;
+
+    //Resolve method and arguments
+      $methodName = $args[0]; //The first argument you pass is the method name.
+
+      $params = [];
+      for($i = 1; $i < count($args); $i++)
+        $params[] = $args[$i];
+
+      if(substr($methodName, 0, 2) == "::")
+        $methodName = substr($methodName, 2);
+      else
+        $className = new $className;
+
+      return call_user_func_array(array($className, $methodName), $params);
+
+
+  }
+
+  public static function autoResolveClassname($cacheBaseDir, $recursiveBaseDir, $appName, $class)
+  {
+
+    $namespaces = [];
+
+    $dir = scandir($recursiveBaseDir);
+
+    for($i = 2; $i < count($dir); $i++) { //skip . and ..
+
+      $newBaseDir = $recursiveBaseDir.$dir[$i];
+
+      if(! is_file($newBaseDir)){
+
+        $newBaseDir .= '\\';
+        static::autoResolveClassname($cacheBaseDir, $newBaseDir, $appName, $class);
+
+      }
+      else{
+
+       $appNamespaced = str_replace($cacheBaseDir, $appName, $newBaseDir);
+       $namespaced = substr($appNamespaced, 0, -4);
+       $className = substr($appNamespaced, strrpos($appNamespaced, '\\') + 1, -4);
+
+       if($class == $className)
+          return $namespaced;
+
+      }
+
+    }
+
+  }
+
+
+}
---
+++ src/Artinict/cusfiguration.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| APP NAME
+|--------------------------------------------------------------------------
+|
+| When you run php artisan app:name, you have to come here and change it manually.
+| The default base namespace is 'App'
+|
+*/
+
+$APP_NAME = 'App\\';
+
+return [
+
+  //This is reserved key name. DO NOT OVERWRITE
+  'LC_APP_NAMESPACE' => $APP_NAME,
+
+  /*
+  |--------------------------------------------------------------------------
+  | User-defined Imports
+  |--------------------------------------------------------------------------
+  |
+  | This is where you should define all your Local Classes, Models, Third-party application (See Vendor prefix below), etc.
+  | Imports defined here overwrites the Laravel Customs automatic imports.Imports must be defined in the format:
+  | 'Alias' => $APP_NAME\Classname::class,
+  |
+  | Note: User defined imports can get really messy in large Apps. Best practice is to use Prefix (See below).
+  |
+  */
+
+  'User' => $APP_NAME.\User::class,
+  
+  
+  
+
+  /*
+  |--------------------------------------------------------------------------
+  | User-defined Imports using Prefix
+  |--------------------------------------------------------------------------
+  |
+  | Prefixes provide a cleaner way to define your import.
+  | It also helps to reduce key name conflict. Prefixes must be defined in the format:
+  | 'Prefix' => [
+  |                 'Alias' => $APP_NAME\Classname::class,
+  |              ],
+  |
+  */
+
+  'Vendors' => [
+
+
+  ],
+
+  'Repo' => [
+
+
+
+  ],
+
+  'Services' => [
+
+
+
+  ],
+
+  /*
+  |--------------------------------------------------------------------------
+  | Laravel's defined Imports
+  |--------------------------------------------------------------------------
+  |
+  | This is where laravel's packages are defined.
+  | You might almost never need to change anything here. Ensure your User-defined prefix doesn't clash with Laravels'.
+  |
+  */
+
+  'Laravels' => [
+
+    'Carbon' => Carbon\Carbon::class,
+  ],
+
+  'Facades' => [
+
+    'App' => Illuminate\Support\Facades\App::class,
+    'Artisan' => Illuminate\Support\Facades\Artisan::class,
+    'Auth' => Illuminate\Support\Facades\Auth::class,
+    'Blade' => Illuminate\Support\Facades\Blade::class,
+    'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
+    'Bus' => Illuminate\Support\Facades\Bus::class,
+    'Cache' => Illuminate\Support\Facades\Cache::class,
+    'Config' => Illuminate\Support\Facades\Config::class,
+    'Cookie' => Illuminate\Support\Facades\Cookie::class,
+    'Crypt' => Illuminate\Support\Facades\Crypt::class,
+    'DB' => Illuminate\Support\Facades\DB::class,
+    'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+    'Event' => Illuminate\Support\Facades\Event::class,
+    'File' => Illuminate\Support\Facades\File::class,
+    'Gate' => Illuminate\Support\Facades\Gate::class,
+    'Hash' => Illuminate\Support\Facades\Hash::class,
+    'Lang' => Illuminate\Support\Facades\Lang::class,
+    'Log' => Illuminate\Support\Facades\Log::class,
+    'Mail' => Illuminate\Support\Facades\Mail::class,
+    'Notification' => Illuminate\Support\Facades\Notification::class,
+    'Password' => Illuminate\Support\Facades\Password::class,
+    'Queue' => Illuminate\Support\Facades\Queue::class,
+    'Redirect' => Illuminate\Support\Facades\Redirect::class,
+    'Redis' => Illuminate\Support\Facades\Redis::class,
+    'Request' => Illuminate\Support\Facades\Request::class,
+    'Response' => Illuminate\Support\Facades\Response::class,
+    'Route' => Illuminate\Support\Facades\Route::class,
+    'Schema' => Illuminate\Support\Facades\Schema::class,
+    'Session' => Illuminate\Support\Facades\Session::class,
+    'Storage' => Illuminate\Support\Facades\Storage::class,
+    'URL' => Illuminate\Support\Facades\URL::class,
+    'Validator' => Illuminate\Support\Facades\Validator::class,
+    'View' => Illuminate\Support\Facades\View::class,
+  ],
+
+
+];
